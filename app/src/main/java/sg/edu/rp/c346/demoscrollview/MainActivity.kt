d894501dea2b4e01bf5b1ec3a087429e03ec6eb2
YAN YIXUAN <17042739@rp.edu.sg>
2019-04-18 11:22:28 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/demoscrollview/MainActivity.kt
@@ -0,0 +1,12 @@
+package sg.edu.rp.c346.demoscrollview
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
